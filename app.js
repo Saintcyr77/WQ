ad01de91ac55448cbab4af5f6ba84b73d9b6f20c
Saintcyr77 <shivam.bhatt04@gmail.com>
2024-02-06 22:26:21 +0530

    cors fix

--- app.js
+++ app.js
@@ -12,4 +12,12 @@ app.listen(port, () => {
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/quotes', getRoute);
